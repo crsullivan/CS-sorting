060649814b5aa74b57baf1d7d2c6ee62039c7c19
Connor Sullivan <crsullivan@protonmail.com>
2020-02-17 13:59:33 -0700

    Tests running and passing for the selection sort, had to comment out trsts for the bubble sort for now for the file to run properly

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,22 +1,24 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# arr = [2,3,0,4,7,8,9,1]
+# print(arr)
+
+def selection_sort( set ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(set) - 1):
         cur_index = i
-        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for j in range(i+1, len(set)):
+            if set[cur_index] > set[j]:
+                cur_index = j
         # TO-DO: swap
+        set[i], set[cur_index] = set[cur_index], set[i]
+    return set
 
+print(selection_sort([7,3,8,1,9,6]))
 
-
-
-    return arr
-
+# print(selection_sort(arr))
+# print(arr)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
